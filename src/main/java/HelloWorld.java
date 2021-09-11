a8c4a213694e0b0b93b7f31a0ffea1be664e9759
universalit5 <90090216+universalit5@users.noreply.github.com>
2021-09-11 15:45:36 +0100

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is my first github commit integrating with jenkinssss");
+        System.out.println("This is my first github commit integrating with jenkins");
     }
 }
