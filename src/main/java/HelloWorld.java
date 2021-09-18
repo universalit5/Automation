b0cf4913557b5f4db55af61b75332293ceffe0b2
universalit5 <90090216+universalit5@users.noreply.github.com>
2021-09-18 07:38:59 +0100

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first github commit integrating with jenkinsssss");
-        System.out.println("This is commit from feature into masterrrr");
+        System.out.println("This is commit from feature into master");
          System.out.println("This demo for no conflict");
         
     }
