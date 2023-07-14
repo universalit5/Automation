c00ca13c687225de1c9675ebbffaf93be6b834ce
universalit5 <90090216+universalit5@users.noreply.github.com>
2023-07-14 15:03:59 +0100

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
          System.out.println("This demo for no conflict");
         System.out.println("This demo ");
         
-   System.outt.printtttln("This demo is for git rebase from eclipse");
+   System.out.println("This demo is for git rebase from eclipse");
 
         
     }
